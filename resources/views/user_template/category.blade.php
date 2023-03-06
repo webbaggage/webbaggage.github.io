a2255f2a68105c676325aed366b509a1e9f57903
FİGEN TEMEL <89043611+figennnnnnn@users.noreply.github.com>
2023-03-07 01:16:56 +0300

    .

---
+++ resources/views/user_template/category.blade.php
@@ -0,0 +1,38 @@
+@extends('user_template.layouts.template')
+@section('main-content')
+<div class="fashion_section">
+         <div id="main_slider" >
+                  <div class="container">
+                     <h1 class="fashion_taital">{{$category->category_name}} - ({{$category->product_count}})</h1>
+                     <div class="fashion_section_2">
+                        <div class="row">
+                           @foreach ($products as $product)
+                           <div class="col-lg-4 col-sm-4">
+                              <div class="box_main">
+                                 <h4 class="shirt_text">{{$product->product_name}}</h4>
+                                 <p class="price_text">Price  <span style="color: #262626;">$ {{$product->price}}</span></p>
+                                 <div class="tshirt_img"><img src="{{ asset($product->product_img)}}"></div>
+                                 <div class="btn_main">
+                                    <div class="buy_bt">
+                                        <form action="{{route('addproducttocart')}}" method="POST">
+                                            @csrf
+                                            <input type="hidden" value="{{ $product->id }}" name="product_id">
+                                            <input type="hidden" value="{{ $product->price }}" name="price">
+                                            <input type="hidden" value="1" name="quantity">
+                                            <br>  
+                                            <br>
+                                            <input class="btn btn-warning" type="submit" value="Buy Now">
+                                        </form>
+                                    </div>
+                                    <div class="seemore_bt"><a href="{{route('singleproduct', [$product->id, $product->slug])}}">See More</a></div>
+                                 </div>
+                              </div>
+                            </div>
+                            @endforeach
+                        </div>
+                     </div>
+                  </div>
+               
+         </div>
+      </div>
+@endsection()
---
+++ resources/views/user_template/userprofile.blade.php
@@ -0,0 +1,5 @@
+
+@extends('user_template.layouts.user_profile_template')
+@section('profilecontent')
+hi
+@endsection
